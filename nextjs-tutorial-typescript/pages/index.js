916ad2ccd73bda72804154f18ecc01603f31d802
SeungGumon <b9327912@gmail.com>
2021-03-30 11:47:57 +0900

    Download Graphql apollo

--- nextjs-tutorial-typescript/pages/index.js
+++ nextjs-tutorial-typescript/pages/index.js
@@ -1,12 +1,25 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    (async () => {
-        const response = await fetch('/api/hello');
-        const json = await response.json()
-        console.log(json);
-        console.log("master git")
-        console.log("git experiment")
-    })()
+    (
+        async () => {
+            const response = await fetch('http://localhost:3000/api/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ query: '{ users { name } }' }),
+            });
+            const json = await response.json();
+            console.log( json );
+        }
+    )();
+
+
+    return (
+        <div className={'container'}>
+            <span>헬로</span>
+        </div>
+    )
 }
+
+
